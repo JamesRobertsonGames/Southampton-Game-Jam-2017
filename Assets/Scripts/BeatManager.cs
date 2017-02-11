74ea6ddf8566e3d472f53bd7abf3d64cb4601dd1
Jamie Robertson <i7291351@bournemouth.ac.uk>
2017-02-11 12:01:46 +0000

    Getting there and added test data

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -4,23 +4,61 @@ using UnityEngine;
 
 public class BeatManager : MonoBehaviour {
 
-    public GameObject Beat;
+    public List<MovingBeat> BeatMap = new List<MovingBeat>();
     public TextAsset textFile;     // drop your file here in inspector
 
     // May inherit this from the other class in Moving Beat. Maybe even the prefab
     [Range(0, 100)]
     public float RateOfTravel = 1;
 
+    public static List<int> Timings = new List<int>();
+
+    bool stillNumber = true;
+
+    int testingbuffer;
+
+
 
     // Use this for initialization
     void Start ()
-    {
+    { 
         string text = textFile.text;  //this is the content as string
-        int.Parse(text);
+        AddtoIntList(text);
+
+        print(Timings[0]);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    public static void AddtoIntList(string str)
+    {
+        foreach (var s in str.Split(','))
+        {
+            if (string.IsNullOrEmpty(str))
+                return;
+
+            int num;
+            if (int.TryParse(s, out num))
+            {
+                Timings.Add(num);
+            }
+
+        }
+
+        if (string.IsNullOrEmpty(str))
+            return;
+
+        return;
+    }
+
+    void SpawnLevel()
+    {
+        // Can't find the size of tarray at the moment
+
+        I
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
 	}
 }
