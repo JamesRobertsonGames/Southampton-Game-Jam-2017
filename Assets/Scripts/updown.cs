76a5f12e09de7b84fd43042fe227b2c33e2ab802
Jamie Robertson <i7291351@bournemouth.ac.uk>
2017-02-12 11:41:54 +0000

    UpandDown Scenery

---
+++ Assets/Scripts/updown.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updown : MonoBehaviour {
+
+    float TimerUpandDown;
+    public float Amplitude;
+    private Vector3 _startPosition;
+
+    public bool opposite; 
+
+    // Use this for initialization
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        TimerUpandDown = Time.deltaTime;
+        
+        if (opposite)
+        {
+            transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.time) * Amplitude, 0.0f);
+        }
+        else
+        {
+            transform.position = _startPosition - new Vector3(0.0f, Mathf.Sin(Time.time) * Amplitude, 0.0f);
+        }
+        
+
+    }
+}
